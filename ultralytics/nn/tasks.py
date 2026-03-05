4a64117e7410152d5f414b94acbd343e65ead71a
UltralyticsAssistant <web@ultralytics.com>
2026-03-05 02:14:10 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import torch
 import torch.nn as nn
 
+from spd_dcnv2 import SPD_DCNv2Down
 from ultralytics.nn.autobackend import check_class_names
 from ultralytics.nn.modules import (
     AIFI,
@@ -25,7 +26,6 @@ from ultralytics.nn.modules import (
     SPPELAN,
     SPPF,
     SPPFFCA,
-    SPPFFCARes,
     A2C2f,
     AConv,
     ADown,
@@ -33,9 +33,9 @@ from ultralytics.nn.modules import (
     BottleneckCSP,
     C2f,
     C2fAttn,
-    C2fMDKA,
-    C2fDWR,
     C2fCIB,
+    C2fDWR,
+    C2fMDKA,
     C2fPSA,
     C3Ghost,
     C3k2,
@@ -67,13 +67,13 @@ from ultralytics.nn.modules import (
     RTDETRDecoder,
     SCDown,
     Segment,
+    SPPFFCARes,
     TorchVision,
     WorldDetect,
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
 )
-from spd_dcnv2 import SPD_DCNv2Down
 from ultralytics.utils import DEFAULT_CFG_DICT, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
 from ultralytics.utils.loss import (
