2eac0a5e46af1d84db7b94d414a7ce3c4d60e755
hehibduhc <13576726251@163.com>
2026-03-12 11:43:27 +0800

    Add YOLOv8-seg lightweight 3-scale BiFPN neck

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -38,6 +38,7 @@ from ultralytics.nn.modules import (
     AConv,
     ADown,
     Bottleneck,
+    BiFPNBlock,
     BottleneckCSP,
     C2f,
     C2fAttn,
@@ -1583,6 +1584,7 @@ def parse_model(d, ch, verbose=True):
             ConvHWDDown,
             C2fCIB,
             A2C2f,
+            BiFPNBlock,
         }
     )
     repeat_modules = frozenset(  # modules with 'repeat' arguments
@@ -1641,6 +1643,9 @@ def parse_model(d, ch, verbose=True):
                     args.extend((True, 1.2))
             if m is C2fCIB:
                 legacy = False
+        elif m is BiFPNBlock:
+            c2 = make_divisible(min(args[0], max_channels) * width, 8)
+            args = [[ch[x] for x in f], c2, *args[1:]]
         elif m is AIFI:
             args = [ch[f], *args]
         elif m in frozenset({HGStem, HGBlock}):
